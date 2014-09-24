bffbbd7595ad13c6910080ccc127cb842f7e41de
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-09-24 16:12:36 -0700

    stop making the report

--- R/makepdf.sh
+++ R/makepdf.sh
@@ -1,5 +1,5 @@
 
 Rscript -e "library(knitr); knit('do_accounts_database.Rmd')"
 ~/.cabal/bin/pandoc --template=tabletemplate.tex do_accounts_database.md --latex-engine=xelatex -o signup_sheet.pdf
-Rscript -e "library(knitr); knit('report_coop_usage.Rmd')"
-~/.cabal/bin/pandoc report_coop_usage.md --latex-engine=xelatex -o report_coop_usage.pdf
+#Rscript -e "library(knitr); knit('report_coop_usage.Rmd')"
+#~/.cabal/bin/pandoc report_coop_usage.md --latex-engine=xelatex -o report_coop_usage.pdf
