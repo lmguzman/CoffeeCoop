0b21bb59e020b7c936a94573f9df49f675c2dda3
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-11-15 10:48:07 -0800

    formatting; add packages

--- R/do_accounts_database.R
+++ R/do_accounts_database.R
@@ -1,6 +1,10 @@
 ## Read in a datasheet of the active co-op users
 accounts_active <- read.csv("../SignupSheet/accounts_active.csv")
 
+library(dplyr)
+library(xtable)
+library(magrittr)
+
 ## convert numbers to text for formatting later
 
 accounts_formatted <- accounts_active %>%
@@ -15,21 +19,16 @@ accounts_formatted <- accounts_active %>%
   cbind(data.frame(Coffee="",Milk="")) %>%
   select(Name,balance_text_format,Coffee,Milk,ID) %>%
   set_names(c("Name","balance","\\textbf{Coffee}","\\textbf{Milk}","ID"))
-#   rename(c("balance_text_format"="balance",
-#            "Coffee"="\\textbf{Coffee}",
-#            "Milk"="\\textbf{Milk}"))
 
-#names(accounts_formatted) <- sprintf("\\Large %s",names(accounts_formatted))
-
-# library(pander)
-# pander(accounts_formatted)
-print(xtable(accounts_formatted,
-             align=c("|","l","|","p{5cm}","|","r","|","p{9cm}","|","p{6cm}","|","l","|")),
-      type='latex',sanitize.text.function=identity,
-      tabular.environment="longtable",hline.after=1:nrow(accounts_formatted),floating=FALSE,
-      include.rownames=FALSE,
-      add.to.row = list(pos = list(0),command = c("\\hline \\endhead ")),
-      comment=FALSE)
+xtable(accounts_formatted,
+       align=c("|","l","|","p{5cm}","|","r","|","p{9cm}","|","p{6cm}","|","l","|")) %>%
+  print(type='latex',sanitize.text.function=identity,
+        tabular.environment="longtable",hline.after=1:nrow(accounts_formatted),floating=FALSE,
+        include.rownames=FALSE,
+        add.to.row = list(pos = list(0),
+                          command = c("\\hline \\endhead ")),
+        comment=FALSE) %>%
+  cat(file = "../SignupSheet/signuptable.tex")
 
 
 ## this ends with the data all nicely organized into a sheet showing Milk and Coffee consumption.
