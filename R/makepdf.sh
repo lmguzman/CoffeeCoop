0f1761b23e6db2c3868c3ae0b00d9aec76342510
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-11-15 12:23:09 -0800

    delete shell script

--- R/makepdf.sh
+++
@@ -1,5 +0,0 @@
-
-Rscript -e "library(knitr); knit('do_accounts_database.Rmd')"
-~/.cabal/bin/pandoc --template=tabletemplate.tex do_accounts_database.md --latex-engine=xelatex --variable fontsize=12pt -o signup_sheet.pdf
-#Rscript -e "library(knitr); knit('report_coop_usage.Rmd')"
-#~/.cabal/bin/pandoc report_coop_usage.md --latex-engine=xelatex -o report_coop_usage.pdf
