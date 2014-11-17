6202e8dc5131ab93332ddf76d59db6739673edef
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-11-17 10:48:30 -0800

    changed name on input trying to work the slider

--- ShinyCoffee/server.R
+++ ShinyCoffee/server.R
@@ -1,7 +1,11 @@
-
+library(dplyr)
 ## load the data
 a_active <- read.csv("../SignupSheet/accounts_active.csv", stringsAsFactors = FALSE)
 
 shinyServer(function(input, output){
-  output$a_active <- renderTable({a_active})
+  output$a_active <- renderTable({a_active %>% 
+                                  #  filter(Printed.Name == input$select_name) %>% 
+                                    filter(balance == input$balance_range)
+                                  })
+  output$output_person <- renderText({paste("searching for", input$select_name)})
 })
--- ShinyCoffee/ui.R
+++ ShinyCoffee/ui.R
@@ -1,13 +1,22 @@
+accts <- read.csv("../SignupSheet/accounts_active.csv", stringsAsFactors = FALSE)
+
 shinyUI(fluidPage(
   titlePanel("BRC coffeh"),
   
   sidebarLayout(
     sidebarPanel("User inputs will be here",
-                 selectInput("select_Printed.Name",
+                 selectInput("select_name",
                              label = "Name",
-                             choices = list("Ada Roman", "Alathea LeTaw"))
+                             choices = unique(accts$Printed.Name)),
+                 sliderInput("balance_range",
+                             label = "the balances",
+                             min = -20,
+                             max = 40,
+                             value = c(-20, 40),
+                             format = "##.##")
                  ),
     mainPanel("My cool graphs will go here",
+              textOutput("output_person"),
               tableOutput("a_active")
               )
     )
