b881222c60833998ab24cde75e01fc3568861470
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-03 12:15:33 -0800

    preliminary Shiny things

--- ShinyCoffee/server.R
+++ ShinyCoffee/server.R
@@ -5,7 +5,9 @@ a_active <- read.csv("../SignupSheet/accounts_active.csv", stringsAsFactors = FA
 shinyServer(function(input, output){
   output$a_active <- renderTable({a_active %>% 
                                   #  filter(Printed.Name == input$select_name) %>% 
-                                    filter(balance == input$balance_range)
+                                    filter(balance > input$balance_range[[1]])
                                   })
-  output$output_person <- renderText({paste("searching for", input$select_name)})
+  output$output_person <- renderText({cat(input$balance_range)
+                                      #cat(str(input$balance_range))
+                                      paste("searching for", input$select_name)})
 })
