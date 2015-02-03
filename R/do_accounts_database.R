91e80923b9219ed37a585859c1b81a240772cbb2
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-03 09:45:03 -0800

    minor formats

--- R/do_accounts_database.R
+++ R/do_accounts_database.R
@@ -20,7 +20,8 @@ accounts_formatted <- accounts_active %>%
   set_names(c("Name","balance","\\textbf{Coffee}","\\textbf{Milk}","ID"))
 
 xtable(accounts_formatted,
-       align=c("|","l","|","p{5cm}","|","r","|","p{9cm}","|","p{6cm}","|","l","|")) %>%
+       align=c("|","l","|","p{5cm}","|","r","|","p{9cm}","|","p{6cm}","|","l","|")
+       ) %>%
   print(type='latex',sanitize.text.function=identity,
         tabular.environment="longtable",hline.after=1:nrow(accounts_formatted),floating=FALSE,
         include.rownames=FALSE,
