74613f3e264e683a4de68c2da827d06114f3b0b2
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-03 09:06:26 -0800

    removing these functions; already in coffee_functions.R

--- R/merge_organize_coop_data.R
+++ R/merge_organize_coop_data.R
@@ -73,48 +73,29 @@
 
 ## otherwise let's go ahead
 
-do_accounts <- function(.money_owed, .money_paid, .people, .goods_bought){
-  
-  left_join(.money_owed, .money_paid) %>%
-    rbind_list(anti_join(.money_paid, .money_owed)) %>%
-    left_join(people) %>% 
-    filter(!Gone) %>%
-    ## some of the remaining (not gone people) have not paid  
-    mutate(paid_total_0 = ifelse(is.na(paid_total), 0, paid_total)) %>%
-    left_join(goods_bought) %>% 
-    mutate(GoodsCredit_0=ifelse(is.na(GoodsCredit),0,GoodsCredit),
-           owing_total_0=ifelse(is.na(owing_total),0,owing_total),
-           balance=GoodsCredit_0+paid_total_0-owing_total_0,
-           balance=ifelse(ID%in%c(18,214),0,balance)) %>%
-    select(ID,Printed.Name,balance) %>% 
-    mutate(Name=Printed.Name) %>% 
-    arrange(Name)
-}
+# do_accounts <- function(.money_owed, .money_paid, .people, .goods_bought){
+#   
+#   left_join(.money_owed, .money_paid) %>%
+#     rbind_list(anti_join(.money_paid, .money_owed)) %>%
+#     left_join(people) %>% 
+#     filter(!Gone) %>%
+#     ## some of the remaining (not gone people) have not paid  
+#     mutate(paid_total_0 = ifelse(is.na(paid_total), 0, paid_total)) %>%
+#     left_join(goods_bought) %>% 
+#     mutate(GoodsCredit_0=ifelse(is.na(GoodsCredit),0,GoodsCredit),
+#            owing_total_0=ifelse(is.na(owing_total),0,owing_total),
+#            balance=GoodsCredit_0+paid_total_0-owing_total_0,
+#            balance=ifelse(ID%in%c(18,214),0,balance)) %>%
+#     select(ID,Printed.Name,balance) %>% 
+#     mutate(Name=Printed.Name) %>% 
+#     arrange(Name)
+# }
 ## and here we can stop the filtering and merging.  accounts_alphabet contains all the info now.
 ##ls()[!ls()%in%"accounts_alphabet"]
 ## we **COULD** remove all the other files but I don't want to.
 
 ## When did people last use the coop?
 
-active_drink <- consumption %>%
-  select(ID,Date) %>%
-  group_by(ID) %>%
-  summarise(lastday=max(Date)) %>%
-  filter(lastday>(now()-dweeks(8))
-  )
-
-
-active_money <- payments %>%
-  mutate(Date=ymd(data_date)) %>%
-  select(ID,Date,data_date) %>% #filter(is.na(Date))
-  group_by(ID) %>%
-  summarise(lastday=max(Date)) %>%
-  filter(lastday > (now() %>% subtract(dweeks(8)))
-  )
-
-active <- rbind(active_drink,active_money) %>%
-  select(ID) %>%
-  unique
 
 accounts_active <- semi_join(accounts,active) %>%
   arrange(Name)
