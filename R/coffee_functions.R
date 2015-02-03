491e148a3647561029a69482b5e56e48f2d3aacc
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-03 10:11:33 -0800

    Moved printing pipline to funtion, with improved code format and feat. correct use of sink and cat

--- R/coffee_functions.R
+++ R/coffee_functions.R
@@ -86,3 +86,20 @@ format_accounts <- . %>%
   mutate(Coffee="", Milk="") %>%
   select(Name, balance_text_format, Coffee, Milk, ID) %>%
   set_names(c("Name","balance","\\textbf{Coffee}","\\textbf{Milk}","ID"))
+
+print_signup_sheet <- function(.accounts_formatted, output){
+  sink(output)
+  xtable(.accounts_formatted,
+         align = c("|","l","|","p{5cm}","|","r","|","p{9cm}","|","p{6cm}","|","l","|")
+  ) %>%
+    print(type = 'latex', sanitize.text.function = identity,
+          tabular.environment = "longtable", 
+          hline.after = 1:nrow(.accounts_formatted),  # note we count the lines here.
+          floating = FALSE, include.rownames=FALSE,
+          add.to.row = list(pos = list(0),
+                            command = c("\\hline \\endhead ")),
+          comment = FALSE) %>%
+    cat
+  sink()
+}
+  
--- R/do_accounts_database.R
+++ R/do_accounts_database.R
@@ -3,16 +3,5 @@
 
 ## convert numbers to text for formatting later
 
-xtable(accounts_formatted,
-       align=c("|","l","|","p{5cm}","|","r","|","p{9cm}","|","p{6cm}","|","l","|")
-       ) %>%
-  print(type='latex',sanitize.text.function=identity,
-        tabular.environment="longtable",hline.after=1:nrow(accounts_formatted),floating=FALSE,
-        include.rownames=FALSE,
-        add.to.row = list(pos = list(0),
-                          command = c("\\hline \\endhead ")),
-        comment=FALSE) %>%
-  cat(file = "../SignupSheet/signuptable.tex")
-
 
 ## this ends with the data all nicely organized into a sheet showing Milk and Coffee consumption.
