6005d7d87e7c2e3449c012d7d1f3283a23f34bca
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-02-03 10:12:08 -0800

    deleted the now-empty do_accounts_database.r file

--- R/do_accounts_database.R
+++
@@ -1,7 +0,0 @@
-## Read in a datasheet of the active co-op users
-
-
-## convert numbers to text for formatting later
-
-
-## this ends with the data all nicely organized into a sheet showing Milk and Coffee consumption.
